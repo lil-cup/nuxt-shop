aac6cc4a92846f4e08aaad47051b3dc0368b1f08
lil cup <mmikkaaaa@gmail.com>
2026-02-19 18:09:30 +0300

    Добавление иконок

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,5 +3,6 @@
     <h1>Welcome to Your Vue.js App</h1>
     <p>This is the main entry point of your application.</p>
     <InputFiled variant="black" placeholder="Введите ваш email для подписки" />
+    <IconTwitter />
   </div>
 </template>
