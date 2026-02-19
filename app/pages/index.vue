2732399da093baf37ba5e7df5d594a70572bf43d
lil cup <mmikkaaaa@gmail.com>
2026-02-19 17:57:37 +0300

    Упражнение - Input

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="app">
-        <h1>Welcome to Your Vue.js App</h1>
-        <p>This is the main entry point of your application.</p>
-    </div>
+  <div class="app">
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>This is the main entry point of your application.</p>
+    <InputFiled variant="black" placeholder="Введите ваш email для подписки" />
+  </div>
 </template>
