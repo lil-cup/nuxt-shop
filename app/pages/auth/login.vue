18dc86e261ca4c2bb5b4c15d65d8c44051179ccf
lil cup <mmikkaaaa@gmail.com>
2026-03-18 18:58:01 +0300

    Форма авторизации

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -6,7 +6,49 @@ definePageMeta({
 
 <template>
   <div class="app">
-    <h1>Welcome to Your Vue.js App</h1>
-    <p>This is the main entry point of your application.</p>
+    <h1>Мой аккаунт</h1>
+    <form class="login-form">
+      <div class="login-form__fileds">
+        <InputFiled variant="gray" placeholder="Email" />
+        <InputFiled variant="gray" placeholder="Пароль" type="password" />
+      </div>
+      <div class="login-form__actions">
+        <ActionButton>Вход</ActionButton>
+        <NuxtLink to="/auth/restore">Забыли пароль?</NuxtLink>
+      </div>
+    </form>
   </div>
 </template>
+
+<style scoped>
+.login-form {
+  display: flex;
+  flex-direction: column;
+  gap: 70px;
+  max-width: 500px;
+  margin: 0 auto;
+  margin-top: 64px;
+}
+
+.login-form__fileds {
+  display: flex;
+  flex-direction: column;
+  gap: 46px;
+}
+
+.login-form__actions {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
+.login-form__actions a {
+  text-decoration: none;
+  margin: 0 auto;
+  color: var(--color-black);
+}
+
+.login-form__actions a:hover {
+  color: var(--color-black-hover);
+}
+</style>
