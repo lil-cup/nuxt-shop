5bad2aef40c63481054f184214a05fa7c342b1b2
lil cup <mmikkaaaa@gmail.com>
2026-03-18 18:29:44 +0300

    Отображение избранного

--- app/pages/favorites.vue
+++ app/pages/favorites.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+import type { Product } from "~/types/product";
+
 const favoriteStore = useFavoritesStore();
+const config = useRuntimeConfig();
+const API_URL = config.public.api_url;
+const products = ref<Product[]>();
+
+watchEffect(async () => {
+  const data = await Promise.all(
+    favoriteStore.favoriteIds.map((id) => {
+      return $fetch<{ product: Product }>(`${API_URL}/products/${id}`);
+    }),
+  );
+
+  products.value = data.map((el) => el.product);
+});
 </script>
 
 <template>
-  <div>{{ favoriteStore.favoriteIds[0] }}</div>
+  <div class="catalog__grid">
+    <CatalogGard
+      v-for="product in products"
+      :key="product.id"
+      v-bind="product"
+    />
+  </div>
 </template>
+
+<style scoped>
+.catalog__grid {
+  display: flex;
+  width: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  gap: 64px 12px;
+  flex-wrap: wrap;
+}
+</style>
