07bec914b3c8d2e5dbff66f45e5e816e79a21e70
lil cup <mmikkaaaa@gmail.com>
2026-03-23 18:32:01 +0300

    Nuxt DevTools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   runtimeConfig: {
     public: {
       api_url: "",
