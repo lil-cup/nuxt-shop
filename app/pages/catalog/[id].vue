98b3178aa14e70e2c6f0e71e3792b67da1bc7d3f
lil cup <mmikkaaaa@gmail.com>
2026-03-23 17:23:45 +0300

    Динамический title

--- app/pages/catalog/[id].vue
+++ app/pages/catalog/[id].vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import type { Product } from "~/types/product";
+const route = useRoute();
+const API_URL = useAPI();
+
+const { data } = await useFetch<{ product: Product }>(
+  `${API_URL}/products/${route.params.id}`,
+);
+
+useSeoMeta({
+  title: `Купить - ${data.value?.product.name}`,
+  description: data.value?.product.short_description,
+});
+</script>
+
 <template>
-  <div class="app">
-    <h1>Welcome to Your Vue.js App</h1>
-    <p>This is the main entry point of your application.</p>
-  </div>
+  <div>{{ data?.product.name }}</div>
 </template>
